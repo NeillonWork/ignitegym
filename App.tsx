a2435ba84c340460435a04c715267eb50b912ebc
Admin <neillonwork@gmail.com>
2024-08-26 20:58:39 -0300

    add assets

--- App.tsx
+++ App.tsx
@@ -1,4 +1,4 @@
-import { Text, View } from "react-native";
+import { Text, View, StatusBar } from "react-native";
 import {
   Roboto_400Regular,
   Roboto_700Bold,
@@ -9,12 +9,21 @@ export default function App() {
   const [fontsLoaded] = useFonts({ Roboto_700Bold, Roboto_400Regular });
 
   return (
-    <View style={{ flex: 1, alignItems: "center", justifyContent: "center" }}>
-      {fontsLoaded ? (
-        <Text>Home</Text>
-      ) : (
-        <View />
-      )}
+    <View
+      style={{
+        flex: 1,
+        alignItems: "center",
+        justifyContent: "center",
+        backgroundColor: "#202024",
+      }}
+    >
+      <StatusBar
+        barStyle="light-content"
+        backgroundColor="transparent"
+        translucent
+      />
+
+      {fontsLoaded ? <Text>Home</Text> : <View />}
     </View>
   );
 }
