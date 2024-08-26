3c0cd1bd878652e42325d99f4b1594c8dce12931
Admin <neillonwork@gmail.com>
2024-08-26 20:53:13 -0300

    ajustado versao do typescript e add fonte roboto

--- App.tsx
+++ App.tsx
@@ -1,12 +1,20 @@
-import { StatusBar } from "expo-status-bar";
 import { Text, View } from "react-native";
-import { useFonts } from "@expo-google-fonts/roboto";
+import {
+  Roboto_400Regular,
+  Roboto_700Bold,
+  useFonts,
+} from "@expo-google-fonts/roboto";
 
 export default function App() {
+  const [fontsLoaded] = useFonts({ Roboto_700Bold, Roboto_400Regular });
+
   return (
     <View style={{ flex: 1, alignItems: "center", justifyContent: "center" }}>
-      <Text>Home</Text>
-      <StatusBar style="auto" />
+      {fontsLoaded ? (
+        <Text>Home</Text>
+      ) : (
+        <View />
+      )}
     </View>
   );
 }
