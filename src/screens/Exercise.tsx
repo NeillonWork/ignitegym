beb979589e095fe65e175db7c16e99500fe6671c
Admin <neillonwork@gmail.com>
2024-09-23 17:43:57 -0300

    ajustando BottomTab

--- src/screens/Exercise.tsx
+++ src/screens/Exercise.tsx
@@ -4,7 +4,7 @@ export function Exercise(){
 
     return(
         <Center flex={1}>
-            <Text>Exercise</Text>
+            <Text color="$white">Exercise</Text>
         </Center>
     )
 }
