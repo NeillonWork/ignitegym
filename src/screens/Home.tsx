2dce258eae1f8a295e1cade155f5ee7c3dd3193b
Admin <neillonwork@gmail.com>
2024-09-30 17:24:29 -0300

    Criado lista vertical de exercicios

--- src/screens/Home.tsx
+++ src/screens/Home.tsx
@@ -1,10 +1,38 @@
+import { useState } from "react";
+
+import { Group } from "@components/Group";
 import { HomeHeader } from "@components/HomeHeader";
 import { VStack } from "@gluestack-ui/themed";
+import { FlatList } from "react-native";
 
 export function Home() {
+  const [groups, setGroups] = useState([
+    "Costas",
+    "Biceps",
+    "Triceps",
+    "Ombro",
+  ]);
+  const [groupSelected, setGroupSelected] = useState("Costas");
+
   return (
     <VStack flex={1}>
       <HomeHeader />
+
+      <FlatList
+        data={groups}
+        keyExtractor={(item) => item}
+        renderItem={({ item }) => (
+          <Group
+            name={item}
+            isActive={groupSelected === item}
+            onPress={() => setGroupSelected(item)}
+          />
+        )}
+        horizontal
+        showsHorizontalScrollIndicator={false}
+        contentContainerStyle={{ paddingHorizontal: 32 }}
+        style={{ marginVertical: 40, maxHeight: 44, minHeight: 44 }}
+      />
     </VStack>
   );
 }
