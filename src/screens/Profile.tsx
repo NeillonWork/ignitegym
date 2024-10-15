0d892de637b624b81d343faea5c0b96b8b0ae940
Admin <neillonwork@gmail.com>
2024-10-15 18:13:21 -0300

    acesso a galeria de imagens do profile Feito!

--- src/screens/Profile.tsx
+++ src/screens/Profile.tsx
@@ -4,8 +4,29 @@ import { Center, Heading, Text, VStack } from "@gluestack-ui/themed";
 import { ScrollView, TouchableOpacity } from "react-native";
 import { Input } from "@components/input";
 import { Button } from "@components/Button";
+import * as ImagePicker from "expo-image-picker";
+import { useState } from "react";
 
 export function Profile() {
+  const [userPhoto, setUserPhoto] = useState(
+    "https:github.com/NeillonWork.png"
+  );
+
+  async function handleUserPhotoSelect() {
+    const photoSelected = await ImagePicker.launchImageLibraryAsync({
+      mediaTypes: ImagePicker.MediaTypeOptions.Images,
+      quality: 1,
+      aspect: [4, 4],
+      allowsEditing: true,
+    });
+
+    if (photoSelected.canceled) {
+      return;
+    }
+
+    setUserPhoto(photoSelected.assets[0].uri);
+  }
+
   return (
     <VStack flex={1}>
       <ScreenHeader title="Perfil" />
@@ -13,11 +34,11 @@ export function Profile() {
       <ScrollView contentContainerStyle={{ paddingBottom: 36 }}>
         <Center mt="$6" px="$10">
           <UserPhoto
-            source={{ uri: "https:github.com/NeillonWork.png" }}
+            source={{ uri: userPhoto }}
             alt="Foto do usuario"
             size="xl"
           />
-          <TouchableOpacity>
+          <TouchableOpacity onPress={handleUserPhotoSelect}>
             <Text
               color="$green500"
               fontFamily="$heading"
@@ -45,11 +66,15 @@ export function Profile() {
             Alterar senha
           </Heading>
 
-          <Center  w="$full" gap="$4">
-            <Input placeholder="Senha antiga" bg="$gray600" secureTextEntry/>
-            <Input placeholder="Nova senha" bg="$gray600" secureTextEntry/>
-            <Input placeholder="Confirme a nova senha" bg="$gray600" secureTextEntry/>
-            <Button title="Atualizar"/>
+          <Center w="$full" gap="$4">
+            <Input placeholder="Senha antiga" bg="$gray600" secureTextEntry />
+            <Input placeholder="Nova senha" bg="$gray600" secureTextEntry />
+            <Input
+              placeholder="Confirme a nova senha"
+              bg="$gray600"
+              secureTextEntry
+            />
+            <Button title="Atualizar" />
           </Center>
         </Center>
       </ScrollView>
