3339a596d6868c892963a9877e629880eeebb18c
Admin <neillonwork@gmail.com>
2024-10-21 18:41:54 -0300

    Adicionado plugin de File Size

--- src/screens/Profile.tsx
+++ src/screens/Profile.tsx
@@ -1,11 +1,13 @@
+import { useState } from "react";
+import { Alert, ScrollView, TouchableOpacity } from "react-native";
+import * as FileSystem from "expo-file-system";
+import * as ImagePicker from "expo-image-picker";
+
+import { Center, Heading, Text, VStack } from "@gluestack-ui/themed";
 import { ScreenHeader } from "@components/ScreenHeader";
 import { UserPhoto } from "@components/UserPhoto";
-import { Center, Heading, Text, VStack } from "@gluestack-ui/themed";
-import { ScrollView, TouchableOpacity } from "react-native";
 import { Input } from "@components/input";
 import { Button } from "@components/Button";
-import * as ImagePicker from "expo-image-picker";
-import { useState } from "react";
 
 export function Profile() {
   const [userPhoto, setUserPhoto] = useState(
@@ -24,7 +26,25 @@ export function Profile() {
       return;
     }
 
-    setUserPhoto(photoSelected.assets[0].uri);
+    const photoURI = photoSelected.assets[0].uri;
+
+    setUserPhoto(photoURI);
+
+    if (photoURI) {
+      const photoInfo = (await FileSystem.getInfoAsync(photoURI)) as {
+        size: number;
+      };
+
+      if (photoInfo.size && photoInfo.size / 1024 / 1024 > 5) {
+        return Alert.alert(
+          "Essa imagem é muito grande. Escolha uma de até 5MB"
+        );
+
+        return;
+      }
+
+      setUserPhoto(photoURI);
+    }
   }
 
   return (
